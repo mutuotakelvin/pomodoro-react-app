d1dd110741d066ed36a968b6d6cb56fb7bfce96e
mutuotakelvin <kelvinmutuota749@gmail.com>
2022-01-23 03:56:25 +0300

    Refactor

--- src/components/Credits/Credits.jsx
+++ src/components/Credits/Credits.jsx
@@ -1,16 +1,10 @@
 import './credits.sass';
-import { useStateValue } from "../../contexts/stateProvider";
 
 const Credits = () => {
-
-    const [{githubProfileUrl, githubUsername}] = useStateValue();
-
     return (
         <h5 class="credits__block">
             Developed with ❤️ by&nbsp;
-            <a class="credits__link" href={githubProfileUrl}>
-                {githubUsername}
-            </a>
+                kelvin Mutuota Mwangi
         </h5>
     )
 }
